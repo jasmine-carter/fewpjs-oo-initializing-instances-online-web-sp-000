1dc3ea77dca9898d9c16225d9082cce470d54eb1
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-13 18:15:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Write your code here
-class Breakfast = {}
+class Breakfast = {
+  constructor(food, drink) {
+    this.drink = drink ;
+    this.food = food ;
+  }
+}
 class Lunch = {}
 class Dinner = {}
